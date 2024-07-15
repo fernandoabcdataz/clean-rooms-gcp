3efe4cc9df2758bf91454aaf2a07d9640f5250c5
Fernando Maximo Ferreira <fernandomaximoferreira@Fernandos-MacBook-Pro.local>
2024-07-15 21:29:12 +1200

    created new directories for iag and trademe teerraform

---
+++ infra-client-v2/iag/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.36.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = "australia-southeast1"
+  credentials = "/Users/fernandomaximoferreira/.gcloud/service-account-key.json"
+}
